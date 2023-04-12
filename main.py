e241201cff55d919b7dbdc961b23b24afb4fd683
brenerkennedy <130412852+brenerkennedy@users.noreply.github.com>
2023-04-12 07:30:21 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+ano_atual = int(input("Em que ano nós estamos? "))
+idade = int(input("Qual é a sua idade? "))
+ano_futuro = int(input("Digite um ano futuro "))
+nome = input("Qual é o seu nome?")
+idade_futura = ano_futuro - ano_atual + idade
+print(nome,"No ano de",ano_futuro," você tera:",idade_futura,"anos de idade" )
